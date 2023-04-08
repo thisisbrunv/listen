6a44f49fca7110b8ae8d6d9683cb13484f07284f
Carlos Holderried <chcholderried@gmail.com>
2023-04-08 17:59:28 -0300

    classes

--- app/src/main/java/com/example/listen/MainActivity.java
+++ app/src/main/java/com/example/listen/MainActivity.java
@@ -3,14 +3,30 @@ package com.example.listen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listaMusicas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaMusicas = findViewById(R.id.lista_musicas);
+
+        List<Musica> musicas = getMusicasArmazenadas(this);
+
+        MusicaAdapter adapter = new MusicaAdapter(this, musicas);
+        listaMusicas.setAdapter(adapter);
+
     }
+
+
+
 
     /*
     VERIFICAR SE FOI GARANTIDO A PERMISSÃO AOS ARQUIVOS

361f18f5a852b0886fbcb125e5ab890d578dce8a
bruna schneider <brunaks26@gmai.com>
2023-04-08 17:33:38 -0300

    PERMISSÕES

--- app/src/main/java/com/example/listen/MainActivity.java
+++ app/src/main/java/com/example/listen/MainActivity.java
@@ -11,4 +11,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*
+    VERIFICAR SE FOI GARANTIDO A PERMISSÃO AOS ARQUIVOS
+
+    if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                REQUEST_READ_EXTERNAL_STORAGE);
+    }
+
+     */
+
 }
